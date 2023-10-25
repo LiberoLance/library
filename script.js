29ebc37f788a843eff1042576ed98ee4bc6e546a
Libero Lance <mfc2294@gmail.com>
2023-10-25 13:55:11 -0400

    write book object definition

---
+++ script.js
@@ -0,0 +1,18 @@
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  read() {
+    if(this.read) {
+      this.read = false;
+    }
+    else {
+      this.read = true;
+    }
+  }
+}
+
