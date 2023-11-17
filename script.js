e038fb65bcb1333c0ff16fd182618f2131aa28ae
Libero Lance <mfc2294@gmail.com>
2023-11-16 20:29:27 -0500

    add change read status and remove functions

--- script.js
+++ script.js
@@ -32,9 +32,67 @@ addBook.addEventListener("click", function() {
 });
 
 showBooks.addEventListener("click", function() {
-  for(let thisBook of library) {
-    console.log(thisBook.title);
+  const bookContainer = document.querySelector('#bookc');
+  let div;
+  let title;
+  let author;
+  let pages;
+  let read;
+  let changeReadBtn;
+  let removeBtn;
+
+  function changeReadStatus() {
+    if(this.previousSibling.textContent == 'true') {
+      this.previousSibling.textContent = 'false';
+    }
+    else {
+      this.previousSibling.textContent = 'true';
+    }
   }
+
+  function remove(){
+    const div = this.parentNode;
+    const title = this.parentNode.childNodes[0].textContent;
+    let theBook;
+    for(let book of library) {
+      if(book.title == title) {
+        theBook = book;
+        break;
+      }
+    }
+    const i = library.indexOf(theBook);
+    if(i > -1) {
+      library.splice(i, 1);
+    }
+    div.remove();
+    index--;
+    console.log(library);
+  }
+
+  for(; index < library.length; index++) {
+    div = document.createElement('div');
+    title = document.createElement('p');
+    author = document.createElement('p');
+    pages = document.createElement('p');
+    read = document.createElement('p');
+    changeReadBtn = document.createElement('button');
+    changeReadBtn.setAttribute('id', index);
+    removeBtn = document.createElement('button');
+    removeBtn.setAttribute('id', index);
+
+    title.textContent = library[index].title;
+    author.textContent = library[index].author;
+    pages.textContent = library[index].pages;
+    read.textContent = library[index].read;
+    changeReadBtn.textContent = 'Change Read Status';
+    changeReadBtn.addEventListener('click', changeReadStatus);
+    removeBtn.textContent = 'Remove Book';
+    removeBtn.addEventListener('click', remove);    
+
+    div.append(title, author, pages, read, changeReadBtn, removeBtn);
+    bookContainer.append(div);
+  }
+
 });
 
 //functions
